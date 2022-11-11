4331f2cdc064a478d7f23ffcb5b75a262105a1c8
Carolina <caroyabril2019@gmail.com>
2022-11-10 21:58:16 -0300

    documento

--- src/main/java/com/cimexstock/cimexstock/CimexstockApplication.java
+++ src/main/java/com/cimexstock/cimexstock/CimexstockApplication.java
@@ -9,6 +9,13 @@ import java.sql.SQLException;
 public class CimexstockApplication {
 
 	public static void main(String[] args) throws SQLException {
+		//UsuarioServlet usuarioServlet=new UsuarioServlet();
+		//System.out.println(usuarioServlet.consultarUsuario(0));
+		//usuarioServlet.addUsuario(new Usuario(2,"Maria", "San Martin 234", "Maria", 85844,"123"));
+		//List<Usuario> usuarios=usuarioServlet.listartarUsuario();
+		//System.out.println(usuarios);
+		//usuarios.get(2).setNombre("Maria Catalina");
+		//System.out.println(usuarioServlet.modificarUsuario(usuarios.get(2)));
 
 		 LoguinController servlet = new LoguinController();
 		 servlet.Loguin();
